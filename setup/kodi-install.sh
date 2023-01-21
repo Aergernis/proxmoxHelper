d69a6dc8e3d1611dee1a8acbbbb21d346a000fd4
Aergernis <all.forum.activations@gmail.com>
2023-01-21 11:20:31 +0100

    Update kodi-install.sh

--- setup/kodi-install.sh
+++ setup/kodi-install.sh
@@ -73,6 +73,8 @@ msg_info "Installing Dependencies"
 apt-get install -y curl &>/dev/null
 apt-get install -y sudo &>/dev/null
 apt-get install -y gnupg &>/dev/null
+apt-get install -y software-properties-common &>/dev/null
+add-apt-repository -y ppa:team-xbmc/ppa
 msg_ok "Installed Dependencies"
 
 msg_info "Setting Up Hardware Acceleration"  
